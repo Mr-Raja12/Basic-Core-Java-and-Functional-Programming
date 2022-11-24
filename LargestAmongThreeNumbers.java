c63ecedda4dceb42d0e9dcd3e182ba5aa1bcc9f7
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-24 15:17:21 +0530

    Added a Program to find Largest Among Three Number

---
+++ LargestAmongThreeNumbers.java
@@ -0,0 +1,19 @@
+package com.bl.corejavaprogram;
+public class LargestAmongThreeNumbers {
+	  public static void main(String[] args) {
+	        int number1=10,number2=20,number3=30;
+	        if(number1>=number2 && number3>=number3)
+	        {
+	            System.out.println("Greatest Number is "+number1);
+	        }
+	        else if (number2>=number1 && number2>=number3)
+	        {
+	            System.out.println("Greatest Number is " +number2);
+	        }
+	        else if (number3>=number1 && number3>=number2)
+	        {
+	            System.out.println("Greatest Number is " +number3);
+
+	        }
+      }
+ }
